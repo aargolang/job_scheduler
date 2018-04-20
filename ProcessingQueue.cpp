9aab2f3ecadb6663e06c342a9880fe92199d2e29
Arthur Lang <aargolang@gmail.com>
2018-04-19 21:02:55 -0700

    fixing things

---
+++ ProcessingQueue.cpp
@@ -0,0 +1,15 @@
+#include "ProcessingQueue.h"
+// #include "Job.h"
+
+ProcessingQueue::ProcessingQueue(){
+
+}
+ProcessingQueue::~ProcessingQueue(){
+	
+}
+void ProcessingQueue::insertJob(Job j){
+	heap.push(j);
+}
+Job ProcessingQueue::findShortest(){
+	return heap.top();
+}
---
+++ ProcessingQueue.h
@@ -0,0 +1,35 @@
+#ifndef PROCQUEUE 
+#define PROCQUEUE
+
+#include <queue>
+#include <vector>
+#include <functional>
+#include "Job.h"
+
+using std::priority_queue;
+using std::vector;
+using std::greater;
+
+struct compareEnd
+{
+	bool operator()(const Job lhs, const Job rhs) const
+	{
+		return lhs.end_time > rhs.end_time;
+	}
+};
+
+class ProcessingQueue
+{
+public:
+	ProcessingQueue();
+	~ProcessingQueue();
+	void processJob(Job j);
+	void retireJob();	
+	Job findShortest();
+
+private:
+	// from: https://stackoverflow.com/questions/2439283/how-can-i-create-min-stl-priority-queue
+	priority_queue<Job, vector<Job>, compareEnd > heap;
+};
+
+#endif
